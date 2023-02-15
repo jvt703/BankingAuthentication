fd8f7ace10b8090aaae94161ab7ddceb293b6f17
John <jvt703@gmail.com>
2023-02-14 20:03:59 -0500

    N1T-71 JWTservice methods create

--- src/main/java/com/authentication/authentication/config/JwtService.java
+++ src/main/java/com/authentication/authentication/config/JwtService.java
@@ -2,17 +2,31 @@ package com.authentication.authentication.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-
+    private static final String SECRET_KEY = "7A25432A462D4A614E645267556B58703273357638792F413F4428472B4B6250";
     public String extractUsername(String token) {
         return null;
     }
 
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
