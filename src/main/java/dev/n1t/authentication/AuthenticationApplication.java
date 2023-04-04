b80cfb3b3f62d8d5b4481ef498b47172b0c8a729
John <jvt703@gmail.com>
2023-04-04 15:39:12 -0400

    Eureka client addition

--- src/main/java/dev/n1t/authentication/AuthenticationApplication.java
+++ src/main/java/dev/n1t/authentication/AuthenticationApplication.java
@@ -2,7 +2,9 @@ package dev.n1t.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticationApplication {
 
